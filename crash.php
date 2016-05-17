bc9da2a9566d91478d237bba6e2b83f2388931d9
Jamie Reid <jamie.reid@gmx.com>
2016-05-17 07:51:06 +0200

    fixes

--- crash.php
+++ crash.php
@@ -1,5 +1,5 @@
 <?php require_once( 'update/cms.php' ); ?>
-<cms:template title='404' executable='1' order='2'  />
+<cms:template title='500' executable='1' order='2'  />
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html lang="en">
 <head>
